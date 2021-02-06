9ab1d8eacf82aaf9cb435bb25359d4a97776c1c2
CDSBeef <calincomptonsilva@outlook.com>
2021-02-06 13:24:26 +0000

    Calins project from spring

---
+++ src/main/java/com/testerforcalin/Calin/CalinApplication.java
@@ -0,0 +1,13 @@
+package com.testerforcalin.Calin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalinApplication.class, args);
+	}
+
+}
